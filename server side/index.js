266cd592a577fb2dec3f10e4249fe831d0785104
viswath-vs <viswathleon@gmail.com>
2021-06-12 13:08:08 +0530

    Base Architecture completed

---
+++ server side/index.js
@@ -0,0 +1,21 @@
+import app from './server.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// variable configurations
+dotenv.config();
+const port = process.env.PORT || 5000;
+const url = process.env.DATABASE_LINK;
+
+// connecting to database .
+mongoose
+    .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then(app.listen(port, () => console.log('database connected server is running at port 5000')))
+    .catch((err) => {
+        console.log(err);
+    });
---
+++ server side/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import request from './api/request.js';
+
+// intializing variables
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// handling all api request from front end
+app.use('/api', request);
+
+// returning error not found
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
+
+export default app;
